4a0e1b6c1dba80a087957f2f85d6626df0486d8c
iyiola <aluiyiola40@gmail.com>
2026-09-24 15:56:27 +0100

    Added more products

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        // You can add a pathname pattern if you want to be more specific, e.g.:
-        // pathname: '/your-cloud-name/**',
-      },
-    ],
-  },
-};
+    images: {
+        remotePatterns: [
+            { protocol: "https", hostname: "res.cloudinary.com" },
+            { protocol: "https", hostname: "images.unsplash.com" },
+        ],
+    },
+}
 
-export default nextConfig;
+export default nextConfig
